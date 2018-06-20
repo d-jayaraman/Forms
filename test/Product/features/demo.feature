661b0b41a5293da8146ea853b8ca9c76bb901b8d
d-jayaraman <djayaraman@tapqa.com>
2018-06-20 13:58:00 -0500

    all good

--- test/Product/features/demo.feature
+++ test/Product/features/demo.feature
@@ -3,5 +3,5 @@ Feature: Web Demo
 @CancelPending
 Scenario: Verify Cancel Pending XML
   Given Policy number is "110801104017001" for "Cancel Pending"
-  Then Verify Policy number "dm"
+  Then Verify Policy number
   And Verify company name is "Infinity"
--- test/common/steps/common.steps.js
+++ test/common/steps/common.steps.js
@@ -12,35 +12,27 @@ Given(/^Policy number is "(.*?)" for "(.*?)"$/, (upolnum, scenario) => {
 
   return new Promise(function(resolve, reject) {
 
-  var filename = upolnum + '_' + scenario + ' XML.xml';
-  var fs = require('fs'),
-    path = require('path'),
-    DOMParser = require('xmldom').DOMParser,
-    filePath = path.join('C:/Users/jayarad/Desktop/XML/Code/', filename);
-      fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) //https://stackoverflow.com/questions/18386361/read-a-file-in-node-js
-        {
-        if (!err) {
-          resolve(
-            global.xmldata = new DOMParser().parseFromString(data,'text/xml'));
-            } else {  reject(err);  }
-        });
+    var filename = upolnum + '_' + scenario + ' XML.xml';
+    var fs = require('fs'),
+      path = require('path'),
+      DOMParser = require('xmldom').DOMParser,
+      filePath = path.join('C:/Users/jayarad/Desktop/XML/Code/', filename);
+        fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) //https://stackoverflow.com/questions/18386361/read-a-file-in-node-js
+          { if (!err) {
+              resolve(global.xmldata = new DOMParser().parseFromString(data,'text/xml'));
+              } else {  reject(err);  }
+          }
+        );
   })
 });
 
 Then(/^Verify company name is "(.*?)"$/, (compid) => {
-
-
     const companyname = xmldata.getElementsByTagName("PrintCoName1")[0].childNodes[0].toString();
-    console.log("THEN : Company name is----------------- " + companyname);
+});
 
-      });
-
-Then(/^Verify Policy number "(.*?)"$/, dmy => {
-  global.polid = xmldata.getElementsByTagName("PolicyId")[0].childNodes[0].toString();
-  console.log("Verify policy# is----------------- " + polid);
+Then(/^Verify Policy number$/, () => {
   assert.equal(xmldata.getElementsByTagName("PolicyId")[0].childNodes[0].toString(), '110801104017001', 'Failed:: Policy ID did not Match' );
-
-  });
+});
 
   /*
   var transactiontinfo = xml.responseXML.getElementsByTagName("TransactionInfo")[0];
