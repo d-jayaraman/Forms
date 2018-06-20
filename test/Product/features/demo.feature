5f145b00c49ef7e0c281b7c40e361e115b90545b
d-jayaraman <djayaraman@tapqa.com>
2018-06-20 15:31:43 -0500

    address block completed

--- test/Product/features/demo.feature
+++ test/Product/features/demo.feature
@@ -4,4 +4,4 @@ Feature: Web Demo
 Scenario: Verify Cancel Pending XML
   Given Policy number is "110801104017001" for "Cancel Pending"
   Then Verify Policy number
-  And Verify company name is "Infinity"
+  And Verify company address
--- test/common/steps/common.steps.js
+++ test/common/steps/common.steps.js
@@ -9,29 +9,37 @@ let pagePath = Constants.getLocatorPath();
  * Common Steps
  */
 Given(/^Policy number is "(.*?)" for "(.*?)"$/, (upolnum, scenario) => {
-
   return new Promise(function(resolve, reject) {
-
-    var filename = upolnum + '_' + scenario + ' XML.xml';
-    var fs = require('fs'),
-      path = require('path'),
-      DOMParser = require('xmldom').DOMParser,
-      filePath = path.join('C:/Users/jayarad/Desktop/XML/Code/', filename);
+    const filename = upolnum + '_' + scenario + ' XML.xml',
+          fs = require('fs'),
+          path = require('path'),
+          DOMParser = require('xmldom').DOMParser,
+          filePath = path.join('C:/Users/jayarad/Desktop/XML/Code/', filename);
         fs.readFile(filePath, {encoding: 'utf-8'}, function(err,data) //https://stackoverflow.com/questions/18386361/read-a-file-in-node-js
           { if (!err) {
-              resolve(global.xmldata = new DOMParser().parseFromString(data,'text/xml'));
+              resolve(
+                global.xmldata = new DOMParser().parseFromString(data,'text/xml'));
+                //console.log(xmldata)
+                //programprofile = xmldata.getElementsByTagName("ProgramProfile")[0];
               } else {  reject(err);  }
           }
         );
   })
 });
 
-Then(/^Verify company name is "(.*?)"$/, (compid) => {
-    const companyname = xmldata.getElementsByTagName("PrintCoName1")[0].childNodes[0].toString();
+Then(/^Verify company address$/, () => {
+    const programprofile = xmldata.getElementsByTagName("ProgramProfile")[0];
+    const compname = programprofile.getElementsByTagName("PrintCoName1")[0].childNodes[0].toString();
+    const compadd1 = programprofile.getElementsByTagName("PrintCoAddr1")[0].childNodes[0].toString();
+    const compadd2 = programprofile.getElementsByTagName("PrintCoAddr2")[0].childNodes[0].toString();
+    console.log(compadd1, compadd2);
+    assert.equal(compname,'Infinity Value Added', 'Failed::Company name is wrong');
+    assert.equal(compadd1,'2201 4th AvenueNorth', 'Failed::Company address is wrong');
 });
 
 Then(/^Verify Policy number$/, () => {
-  assert.equal(xmldata.getElementsByTagName("PolicyId")[0].childNodes[0].toString(), '110801104017001', 'Failed:: Policy ID did not Match' );
+  const polid = xmldata.getElementsByTagName("PolicyId")[0].childNodes[0].toString();
+  assert.equal(polid, '110801104017001', 'Failed::Policy ID did not Match');
 });
 
   /*
